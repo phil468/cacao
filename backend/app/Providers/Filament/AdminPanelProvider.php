50f2359a3a9e5d3f41e90a7891ad6bec66c68252
JOHNDELACRUZ <jdelacruzcalderon@gmail.com>
2026-07-17 18:34:16 -0500

    Add storefront and admin favicons

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('images/brand/favicon/favicon-32.png'))
             ->colors([
                 'primary' => Color::Amber,
             ])
